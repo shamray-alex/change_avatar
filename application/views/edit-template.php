72eb4448d217e42b035a157aaeff271b25a5d51a
alex <shamrayalex84@gmail.com>
2017-02-16 18:02:45 +0200

    next

--- application/views/edit-template.php
+++ application/views/edit-template.php
@@ -33,36 +33,32 @@
 
 
 <script>
-    tinymce.init({
-        selector: "textarea",
-        menubar: false,
-        width: '100%',
-        plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media contextmenu paste code'
-        ],
-        toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-        contextmenu: "cut copy paste | link image inserttable | "
-    });
+    var editorCount = 0;
 
-    var templates = $('#template .template-text-for-edit');
+    //    var templates = $('#template .template-text-for-edit');
     var template = $('#template');
-    var container = document.createElement('div');
-    container.id = 'editableArea';
-    container.className = 'container';
+    var container = $(document.createElement('div'));
+    container.attr('id', 'editableArea');
+    //    container.className = 'container-for-edit';
     template.before(container);
-    templates.each(function (index) {
-        var div = document.createElement('textarea');
-        div.innerHTML = $(this).text();
-        div = $(div);
-//        div.attr('contentEditable', 'true');
-        div.attr('spellcheck', 'true');
-        div.attr('lang', 'en');
-//        div.insertBefore($('#template'));
-        $(container).append(div);
+    //    templates.each(function (index) {
+    //        var div = document.createElement('textarea');
+    //        div.innerHTML = $(this).html();
+    //        div = $(div);
+    //        div.attr('contentEditable', 'true');
+    //        div.attr('spellcheck', 'true');
+    //        div.attr('lang', 'en');
+    //        div.insertBefore($('#template'));
+    //        $(container).append(div);
+    //    });
+
+    var templateChildren = template.children();
+    templateChildren.each(function (index) {
+        var el=$(this);
+        if(el.attr('id')!='headline') {
+            container.html(container.html() + el.html());
+        }
     });
-    template.css('margin-top', '100px');
 
     var textareas = $('#editableArea textarea');
     textareas.each(function () {
@@ -77,6 +73,30 @@
             }
         }
         templates[i].innerHTML = textarea.val();
+    });
+
+    tinymce.init({
+//        selector: "textarea",
+        selector: "#editableArea",
+        menubar: false,
+        width: '100%',
+        browser_spellcheck : true,
+        trim_span_elements: false,
+        verify_html: false,
+        element_format : 'html',
+        plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media paste code',
+//            'contextmenu'
+        ],
+        toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+//        contextmenu: "cut copy paste | link image inserttable | ",
+        setup: function (editor) {
+            editor.on('init', function (a) {
+                console.log('bla-bla');
+            });
+        },
     });
 
     $('.carousel').carousel({
